3d27451f2db705a8111f75ef0e36ff50cc12922a
mirko <mirko@33801692-b1ac-4157-8417-24ff423a0771>
2012-06-06 02:17:47 +0000

    Aggiunto progetto per fare unit testing
    git-svn-id: https://hippias.centropiaggio.unipi.it/RobSoc-Sim/trunk@36 33801692-b1ac-4157-8417-24ff423a0771

---
+++ testing/main.cpp
@@ -0,0 +1,13 @@
+#include <../agent/controller/controller.h>
+
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+	agent_state stato;
+	vector<string> nomi_variabili;
+	
+	//controller c(stato);
+    return 0;
+}
