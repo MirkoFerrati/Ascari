5f119e5d902d1b6b1d38f800519cbe794afe2517
mirko <mirko@33801692-b1ac-4157-8417-24ff423a0771>
2012-06-06 02:17:45 +0000

    Sistemata formattazione del parser
    git-svn-id: https://hippias.centropiaggio.unipi.it/RobSoc-Sim/trunk@35 33801692-b1ac-4157-8417-24ff423a0771

--- shared/yaml_parser.cpp
+++ shared/yaml_parser.cpp
@@ -2,107 +2,107 @@
 
 using namespace std;
 
-std::vector<Parsed_Agent> parse_file(const char * file_name){
-	std::ifstream fin(file_name);
-        std::ostringstream str;
-        str<<fin.rdbuf();
-        std::string new_str=str.str();
-        boost::algorithm::to_upper(new_str);
-        std::stringstream new_new_str;
-        new_new_str<<new_str;
+std::vector<Parsed_Agent> parse_file(const char * file_name) {
+    std::ifstream fin(file_name);
+    std::ostringstream str;
+    str<<fin.rdbuf();
+    std::string new_str=str.str();
+    boost::algorithm::to_upper(new_str);
+    std::stringstream new_new_str;
+    new_new_str<<new_str;
 
 
-        YAML::Parser parser(new_new_str);
-        YAML::Node doc;
-        parser.GetNextDocument(doc);
-	std::vector<Parsed_Agent> Agents(doc.size());
-        for (unsigned i=0;i<doc.size();i++) {
-          doc[i] >> Agents[i];
-	  
-        }
-	  return Agents;
-  
+    YAML::Parser parser(new_new_str);
+    YAML::Node doc;
+    parser.GetNextDocument(doc);
+    std::vector<Parsed_Agent> Agents(doc.size());
+    for (unsigned i=0;i<doc.size();i++) {
+        doc[i] >> Agents[i];
+
+    }
+    return Agents;
+
 }
 
-    
+
 ostream& operator<< (ostream& os, const Parsed_Agent& ag) {
-  
-	    os << ag.name << "\n";
-            for (int i =0 ;i<ag.state.size();i++)
-            {
-                os<< "State:"<<ag.state.at(i)<<endl;
-                os<< "Map: "<<ag.expressions.at(ag.state.at(i))<<endl;
-                os<< "initial value: "<<ag.initial_states.at(ag.state.at(i))<<endl;
-            }
 
-            os << "Controllers" << "\n";
-            int i=0;
-            for (map< controller_name, controller_MapRules >::const_iterator iter=ag.controllers.begin(); iter!=ag.controllers.end();iter++)
-            {
-                i++;
-                os<< "Controller "<<i<<endl;
-                os<< "Name:"<<((*iter).first)<<endl;
-                os<< "Values for Command: "<<endl;
+    os << ag.name << "\n";
+    for (int i =0 ;i<ag.state.size();i++)
+    {
+        os<< "State:"<<ag.state.at(i)<<endl;
+        os<< "Map: "<<ag.expressions.at(ag.state.at(i))<<endl;
+        os<< "initial value: "<<ag.initial_states.at(ag.state.at(i))<<endl;
+    }
 
-                for (int j=0; j<(*iter).second.size();j++)
-                {
-                    os<< ag.inputs[j]<<": "<<((*iter).second.at(j))<<endl;
+    os << "Controllers" << "\n";
+    int i=0;
+    for (map< controller_name, controller_MapRules >::const_iterator iter=ag.controllers.begin(); iter!=ag.controllers.end();iter++)
+    {
+        i++;
+        os<< "Controller "<<i<<endl;
+        os<< "Name:"<<((*iter).first)<<endl;
+        os<< "Values for Command: "<<endl;
 
-                }
-            }
-            os<< "Discrete States: "<<endl;
-            for (map< discreteState_Name, controller_name >::const_iterator iter=ag.discrete_states.begin(); iter!=ag.discrete_states.end();iter++)
-            {
-                os<< "Name:"<<((*iter).first)<<endl;
-                os<< "Controller: "<<((*iter).second)<<endl;
-            }
+        for (int j=0; j<(*iter).second.size();j++)
+        {
+            os<< ag.inputs[j]<<": "<<((*iter).second.at(j))<<endl;
 
-            os<< "Topologies: "<<endl;
-            for (map< Topology_name, Topology_expression >::const_iterator iter=ag.topology_expressions.begin(); iter!=ag.topology_expressions.end();iter++)
-            {
-                os<< "Name:"<<((*iter).first)<<endl;
-                os<< "Expression: "<<((*iter).second)<<endl;
-            }
+        }
+    }
+    os<< "Discrete States: "<<endl;
+    for (map< discreteState_Name, controller_name >::const_iterator iter=ag.discrete_states.begin(); iter!=ag.discrete_states.end();iter++)
+    {
+        os<< "Name:"<<((*iter).first)<<endl;
+        os<< "Controller: "<<((*iter).second)<<endl;
+    }
 
-            os<< "Lambda: "<<endl;
-            for (map< lambda_name, lambda_expression >::const_iterator iter=ag.lambda_expressions.begin(); iter!=ag.lambda_expressions.end();iter++)
-            {
-                os<< "Name:"<<((*iter).first)<<endl;
-                os<< "Expression: "<<((*iter).second)<<endl;
-            }
+    os<< "Topologies: "<<endl;
+    for (map< Topology_name, Topology_expression >::const_iterator iter=ag.topology_expressions.begin(); iter!=ag.topology_expressions.end();iter++)
+    {
+        os<< "Name:"<<((*iter).first)<<endl;
+        os<< "Expression: "<<((*iter).second)<<endl;
+    }
 
-            os<< "Events: "<<endl;
-            for (map< event_name, event_expression >::const_iterator iter=ag.events_expressions.begin(); iter!=ag.events_expressions.end();iter++)
-            {
-                os<< "Event:"<<((*iter).first)<<endl;
-                os<< "Expression: "<<((*iter).second)<<endl;
-            }
+    os<< "Lambda: "<<endl;
+    for (map< lambda_name, lambda_expression >::const_iterator iter=ag.lambda_expressions.begin(); iter!=ag.lambda_expressions.end();iter++)
+    {
+        os<< "Name:"<<((*iter).first)<<endl;
+        os<< "Expression: "<<((*iter).second)<<endl;
+    }
 
-            os<< "Automaton: "<<endl;
-            for (map< discreteState_Name, map< event_name, discreteState_Name > >::const_iterator iter=ag.automaton.begin(); iter!=ag.automaton.end();iter++)
-            {
+    os<< "Events: "<<endl;
+    for (map< event_name, event_expression >::const_iterator iter=ag.events_expressions.begin(); iter!=ag.events_expressions.end();iter++)
+    {
+        os<< "Event:"<<((*iter).first)<<endl;
+        os<< "Expression: "<<((*iter).second)<<endl;
+    }
 
-                string actual_state=((*iter).first);
+    os<< "Automaton: "<<endl;
+    for (map< discreteState_Name, map< event_name, discreteState_Name > >::const_iterator iter=ag.automaton.begin(); iter!=ag.automaton.end();iter++)
+    {
 
-                os<< "Actual State:"<<actual_state<<endl;
+        string actual_state=((*iter).first);
+
+        os<< "Actual State:"<<actual_state<<endl;
 
 
-                for (map< event_name, discreteState_Name >::const_iterator iiter=ag.automaton.at(actual_state).begin(); iiter!=ag.automaton.at(actual_state).end();iiter++)
-                {
-                    os<< "Event:"<<((*iiter).first)<<"->"<<((*iiter).second)<<endl;
+        for (map< event_name, discreteState_Name >::const_iterator iiter=ag.automaton.at(actual_state).begin(); iiter!=ag.automaton.at(actual_state).end();iiter++)
+        {
+            os<< "Event:"<<((*iiter).first)<<"->"<<((*iiter).second)<<endl;
 
-                }
-            }
+        }
+    }
 
 }
 
 
-ostream& operator<< (ostream& os, const std::vector<Parsed_Agent>& ag){
-   for (int i=0; i<ag.size();i++)
-            {
-	    os<<ag.at(i);
-	    os<<endl;
-	    }
+ostream& operator<< (ostream& os, const std::vector<Parsed_Agent>& ag) {
+    for (int i=0; i<ag.size();i++)
+    {
+        os<<ag.at(i);
+        os<<endl;
+    }
 }
 
 
@@ -111,7 +111,7 @@ void operator>> (const YAML::Node& node, Parsed_Agent& ag)
     node["AGENT"]>>ag.name;
     node["STATES"]>>ag.state;
     node["CONTROL_COMMANDS"]>>ag.inputs;
-    
+
     string temp;
     for (int i=0;i<ag.state.size();i++)
     {
@@ -121,7 +121,7 @@ void operator>> (const YAML::Node& node, Parsed_Agent& ag)
         node["INITIAL"][0][ag.state[i]]>>temp;
         ag.initial_states.insert(std::make_pair<stateVariable,initial_state_value>(ag.state[i],temp));
     }
-    
+
     const YAML::Node& behavior= node["BEHAVIOR"][0];
     const YAML::Node& controllers = behavior["CONTROLLERS"];
 
@@ -135,11 +135,7 @@ void operator>> (const YAML::Node& node, Parsed_Agent& ag)
         for (int j=0;j<ag.inputs.size();j++)
         {
             string temp1;
-            controllers[i][ag.inputs[j]]>> temp1;
-
-            ag.controllers[temp][j]=temp1;
-
-
+            controllers[i][ag.inputs[j]]>> ag.controllers[temp][j];
         }
     }
 
@@ -168,7 +164,7 @@ void operator>> (const YAML::Node& node, Parsed_Agent& ag)
     }
 
     const YAML::Node& lambda=encoder["LAMBDA"][0];
-   
+
     lambda["LAMBDA"]>> ag.lambda;
 
 
