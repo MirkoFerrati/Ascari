4a5f7d01662af2e449333956aabe7b9e41306825
mirko <mirko@33801692-b1ac-4157-8417-24ff423a0771>
2012-07-17 15:03:06 +0000

    Rimosso lgf_parser
    git-svn-id: https://hippias.centropiaggio.unipi.it/RobSoc-Sim/trunk@116 33801692-b1ac-4157-8417-24ff423a0771

--- shared/lgf_parser.h
+++ shared/lgf_parser.h
@@ -23,27 +23,6 @@ public:
 
 };
 
-void parseGraph(string filename,Parsed_Graph& p)
-{
-    p.coord_x= new lemon::SmartDigraph::NodeMap<int>(p.graph);
-    p.coord_y= new lemon::SmartDigraph::NodeMap<int>(p.graph);
-    p.length= new lemon::SmartDigraph::ArcMap<int>(p.graph);
-
-    try {
-        lemon::digraphReader(p.graph, filename). // read the directed graph into g
-        nodeMap("coordinates_x", *p.coord_x).	//read the coordinates of nodes
-        nodeMap("coordinates_y", *p.coord_y).	//read the coordinates of nodes
-        arcMap("length", *p.length).       // read the 'capacity' arc map into cap
-        run();
-    } catch (lemon::Exception& er) { // check if there was any error
-       ERR("parsing exception %s",er.what());
-    }
-
-    std::cout << "A digraph is read from "<<filename << std::endl;
-    std::cout << "Number of nodes: " << lemon::countNodes(p.graph) << std::endl;
-    std::cout << "Number of arcs: " << lemon::countArcs(p.graph) << std::endl;
-}
-
 
 
 class lgf_parser
