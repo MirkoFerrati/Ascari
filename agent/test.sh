48dd527a53de004759b96d55c40539bc207f7a7a
mirko <mirko@33801692-b1ac-4157-8417-24ff423a0771>
2012-08-09 10:44:59 +0000

    Aggiunto script di prova con 13 agenti Aggiunto file sorgente modificabile con yEd del megagrafo
    git-svn-id: https://hippias.centropiaggio.unipi.it/RobSoc-Sim/trunk@170 33801692-b1ac-4157-8417-24ff423a0771

---
+++ agent/test.sh
@@ -0,0 +1,14 @@
+./agent AGENTE1 esempio12.yaml &
+./agent AGENTE2 esempio12.yaml &
+./agent AGENTE3 esempio12.yaml &
+./agent AGENTE4 esempio12.yaml &
+./agent AGENTE5 esempio12.yaml &
+./agent AGENTE6 esempio12.yaml &
+./agent AGENTE7 esempio12.yaml &
+./agent AGENTE8 esempio12.yaml &
+./agent AGENTE9 esempio12.yaml &
+./agent AGENTE10 esempio12.yaml &
+./agent AGENTE11 esempio12.yaml &
+./agent AGENTE12 esempio12.yaml &
+./agent AGENTE13 esempio12.yaml &
+
