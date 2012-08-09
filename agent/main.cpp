7a9e559aa6a2495729dc24acfb57cc48b3ccfc56
mirko <mirko@33801692-b1ac-4157-8417-24ff423a0771>
2012-08-09 10:43:46 +0000

    Agent adesso legge da shell il nome del file da caricare
    git-svn-id: https://hippias.centropiaggio.unipi.it/RobSoc-Sim/trunk@167 33801692-b1ac-4157-8417-24ff423a0771

--- agent/main.cpp
+++ agent/main.cpp
@@ -11,12 +11,20 @@ int main(int argc, char **argv) {
     srand(time(NULL));
     LOGOG_INITIALIZE();
     {
+		Parsed_World World;
         logog::Cout out;
-        Parsed_World World=parse_file(FILENAME);
 		if (argc<2)
 		{
 			ERR("%s","please insert the name of the agent");
 			return 1;
+		}
+		if (argc>2)
+		{
+			World=parse_file(argv[2]);
+		}
+		else
+		{
+			World=parse_file(FILENAME);
 		}
         std::string name=argv[1];
         agent a1(name,false,World);
