86c9ae544f8c107af4d56986df76b40908edbf79
mirko <mirko@33801692-b1ac-4157-8417-24ff423a0771>
2012-09-05 14:22:52 +0000

    L'interfaccia dei plugin adesso prevede anche una compileExpressions
    git-svn-id: https://hippias.centropiaggio.unipi.it/RobSoc-Sim/trunk@177 33801692-b1ac-4157-8417-24ff423a0771

--- agent/plugin_module.h
+++ agent/plugin_module.h
@@ -10,6 +10,7 @@ public:
 	virtual void run_plugin()=0;
 	virtual ~Plugin_module(){};
     virtual void addReservedVariables(exprtk::symbol_table< double >& arg1)=0;
+	virtual void compileExpressions(exprtk::symbol_table< double >& arg1)=0;
 };
 
 #endif // PLUGIN_MODULE_H
