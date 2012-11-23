84e7ba7630d658042df493fff70a26b82b737b96
Mirko Ferrati <mirko8787@gmail.com>
2012-11-23 18:30:12 +0100

    Migliorato il collision checker, ancora non so se funziona come dovrebbe

--- simulator/collisionchecker.cpp
+++ simulator/collisionchecker.cpp
@@ -5,6 +5,7 @@
 
 bool CollisionChecker::checkCollisions()
 {
+	bool ret=false;
     for (std::map<std::string,agent_state_packet>::const_iterator it=states.begin();it!=states.end();++it) {
         for (std::map<std::string,agent_state_packet>::const_iterator itt=states.begin();itt!=states.end();++itt) {
             if (it.operator!=(itt))
@@ -12,17 +13,18 @@ bool CollisionChecker::checkCollisions()
                 bool collision=true;
                 for (unsigned int i=0;i<indexes_to_check.size();i++)
                 {
-                    collision=collision &&  (abs(it->second.state.at(i)-itt->second.state.at(i))<COLLISION_RANGE);
+					if (it->second.state.count(i) && itt->second.state.count(i))
+						collision=collision &&  (abs(it->second.state.at(i)-itt->second.state.at(i))<COLLISION_RANGE);
                 }
                 if (collision)
                 {
-                    ERR("Attenzione, c'è stata una collisione tra gli agenti %s e %s",it->first.c_str(),itt->first.c_str());
-                    return true;
+                    ERR("Attenzione, c'e' stata una collisione tra gli agenti %s e %s",it->first.c_str(),itt->first.c_str());
+					ret=true;
                 }
             }
         }
     }
-    return false;
+    return ret;
 }
 
 
@@ -39,4 +41,8 @@ CollisionChecker::CollisionChecker(const agents_name_to_states& states, const in
 		ERR("impossibile effettuare il controllo sugli stati x e y perche' nessun agente possiede quegli stati",NULL);
 		throw "impossibile effettuare il controllo sugli stati x e y perche' nessun agente possiede quegli stati";
 	}
+	if (states_to_index.count("X")) //TODO(Mirko): IssueID #50
+	   indexes_to_check.push_back(states_to_index.at("X"));
+	if (states_to_index.count("Y"))
+	   indexes_to_check.push_back(states_to_index.at("Y"));
 }
