df6b933a3eca1ae63b3b77ac7f717e71d71b152e
Mirko <mirko8787@gmail.com>
2013-01-17 16:16:34 +0100

    Automaton non deterministico

---
+++ agent/automaton/automatonEFSM.cpp
@@ -0,0 +1,32 @@
+#include "automatonEFSM.h"
+
+using namespace std;
+
+automatonEFSM::automatonEFSM (const transitionTable& table) : automatonAbstract (table)
+{
+
+}
+
+
+
+forward_list< automaton_state > automatonEFSM::getNextAutomatonState (const forward_list< automaton_state >& oldStates, const std::map< transition, events::value >& event)
+{
+forward_list<automaton_state> result;
+	//result.push_front(oldStates.front());
+	std::map<transition,automaton_state>::iterator next_state;
+for (auto oldstate=oldStates.begin();oldstate!=oldStates.end();++oldstate)
+{
+	for (map<transition,events::value>::const_iterator it=event.begin(); it!=event.end(); ++it)
+	{ 
+		if (it->second==events::_UNDEFINED || it->second==events::_TRUE) { //Se l'evento e' undefined o vero, NOTA: l'evento deve per forza essere qualcosa!!
+			if ( (next_state=table.internalTable.at(*oldstate).find(it->first)) != table.internalTable.at(*oldstate).end() ){
+				result.push_front(next_state->second);
+				//break; //Commentare questa riga significa rendere l'automaton non deterministico, cosa che qui vogliamo
+			}
+		}
+	}
+}
+result.sort();
+result.unique();
+return result;
+}
