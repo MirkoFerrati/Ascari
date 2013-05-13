be3e4f8ac92f38d820fb39578e3241f4e92a9bef
Mirko <mirko8787@gmail.com>
2013-05-13 17:39:06 +0200

    Scritto l'abstract del parser_plugin

--- plugins/abstract_parser_plugin.h
+++ plugins/abstract_parser_plugin.h
@@ -1,8 +1,27 @@
 #ifndef ABSTRACT_PARSER_PLUGIN_H
 #define ABSTRACT_PARSER_PLUGIN_H
+#include <parserYAML/include/yaml-cpp/node.h>
+
+
+class abstract_parsed_agent_plugin
+{
+public:
+  abstract_parsed_agent_plugin()=delete;
+};
+
+class abstract_parsed_world_plugin
+{
+public:
+  abstract_parsed_world_plugin()=delete;
+};
+
 
 class abstract_parser_plugin
 {
+public:
+  virtual abstract_parsed_agent_plugin* parseAgent(const YAML::Node& node)=0;
+  virtual abstract_parsed_world_plugin* parseWorld(const YAML::Node& node)=0;
+  
 };
 
 #endif // ABSTRACT_PARSER_PLUGIN_H
