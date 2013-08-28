21e2c3bd88289dcaba9cd66def59d9724f7e7779
Alessandro Settimi <ale.settimi@gmail.com>
2013-08-28 16:32:12 +0200

    inserito =0 in  create_objects in abstract_simulator_plugin

--- plugins/abstract_simulator_plugin.h
+++ plugins/abstract_simulator_plugin.h
@@ -10,7 +10,7 @@ public:
   virtual void run_plugin()=0;
   virtual bool initialize(Parsed_World const& w)=0;
   virtual void stop()=0;
-  virtual std::list<abstract_object*>* create_objects(){std::list<abstract_object*>* temp;return temp;};
+  virtual std::list<abstract_object*>* create_objects(){std::list<abstract_object*>* temp=0;return temp;};
   virtual std::string get_objects_type(){return "Error in creating objects";};
   virtual ~abstract_simulator_plugin(){};
 };
