f98686b47a23b83f3c116d688d2abe779ebb131d
Mirko <mirko8787@gmail.com>
2013-09-02 18:01:49 +0200

    il filename di viewer adesso puo' essere letto dal file ascari.cfg

--- viewer/gui/main.cpp
+++ viewer/gui/main.cpp
@@ -67,7 +67,11 @@ int main ( int argc, char *argv[] )
             ERR ( "errore nella lettura dei parametri %s",ex.reason() );
             return 0;
         }
-
+	if (!ap.given("f"))
+	{
+	  filename=CONFIG.getValue("FILENAME");
+  	  INFO("Using %s as filename, read from config file",filename.c_str());
+	}
         QApplication app ( argc,argv );
 
         std::vector<abstract_viewer_plugin*> plugins;
