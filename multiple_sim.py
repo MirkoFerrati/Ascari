75279b4387b5cd8a1ffe873871a80f4cb3a993aa
Mirko <mirko8787@gmail.com>
2014-05-16 18:50:01 +0200

    results are computed after simulations

--- multiple_sim.py
+++ multiple_sim.py
@@ -1,19 +1,30 @@
 import os
 import subprocess
 import shutil
+import glob
+
+agent_min=15
+agent_range=30
+num_simulations=50
 
 root_path = './'
 folderlist=[]
-folders = ['logs_%s' %(x+15) for x in range(30)]
+folders = ['logs_%s' %(x+agent_min) for x in range(agent_range)]
 for folder in folders:
-    os.mkdir(os.path.join(root_path,folder))
     folderlist.append(os.path.abspath(os.path.join(root_path,folder)))
+    os.mkdir(os.path.join(root_path,folder))
     shutil.copy2('ascari.cfg',os.path.join(root_path,folder))
     shutil.copy2('routerwarehousemaxishort.yaml',os.path.join(root_path,folder))
     shutil.copy2('maxiwarehouse.lgf',os.path.join(root_path,folder))
     
-for x in range(30):
+for x in range(agent_range):
     os.chdir(folderlist[x])
-    for i in range(100):
-        subprocess.call(["./../../singleProcess","-n","%s" %(x+15)])
-        #subprocess.call(["echo","-n %s" %(x+15)])
+    for i in range(num_simulations):
+        subprocess.call(["./../../singleProcess","-n","%s" %(x+agent_min)])
+        
+for folder in folderlist:
+    os.chdir(folder)
+    print(folder)
+    files=glob.glob('log_2014-*')
+    print(files)
+    subprocess.check_call(["./../../results"]+files+["-o","test.txt","-s","summary.txt"])
